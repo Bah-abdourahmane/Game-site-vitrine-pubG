3476d2ff43cbf9abb108af4a9930b3715bc98e75
bah-abdourahmane <bahabdourahimane@gmail.com>
2024-05-10 17:06:40 +0100

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="bg-primary text-white py-10 pb-5">
+        <div className="container text-center">
+          <h1 className="text-3xl font-bold">Let&apos;s Play the Game</h1>
+          <hr className="w-[150px] mx-auto my-4" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
